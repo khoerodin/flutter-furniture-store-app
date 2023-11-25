27debf1d11bac5c89279cfc2711d8cc13afec2e2
Khoerodin <heykhoerodin@gmail.com>
2023-11-25 17:49:26 +0700

    Detail Page Part II

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -19,24 +19,157 @@ class DetailPage extends StatelessWidget {
               'assets/image_detail1.png',
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(
-              top: 66,
-              left: 20,
-            ),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: kWhiteColor,
-              border: Border.all(
-                color: kLineDarkColor,
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 66,
+                left: 20,
+              ),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: kWhiteColor,
+                border: Border.all(
+                  color: kLineDarkColor,
+                ),
+              ),
+              child: const Icon(
+                Icons.chevron_left,
               ),
             ),
-            child: const Icon(
-              Icons.chevron_left,
+          ),
+          SizedBox.expand(
+            child: DraggableScrollableSheet(
+              initialChildSize: 0.4,
+              minChildSize: 0.4,
+              maxChildSize: 0.95,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                    color: kWhiteColor,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Center(
+                            child: Container(
+                              width: 30,
+                              height: 4,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: kLineDarkColor,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Poan Chair',
+                                style: blackTextStyle.copyWith(
+                                  fontSize: 24,
+                                  fontWeight: semiBold,
+                                ),
+                              ),
+                              Text(
+                                '\$34',
+                                style: blackTextStyle.copyWith(
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'IKOE',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           )
         ],
+      ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: kWhiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: kGreyColor,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 56,
+              height: 56,
+              margin: const EdgeInsets.only(
+                right: 16,
+              ),
+              decoration: BoxDecoration(
+                color: kWhiteGreyColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: Image.asset(
+                  'assets/icon_cart.png',
+                  width: 24,
+                ),
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: kBlackColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14),
+                    ),
+                  ),
+                  child: Text(
+                    'Buy Now',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
